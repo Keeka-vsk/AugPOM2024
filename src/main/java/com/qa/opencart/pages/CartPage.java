65114c9989fee315997f8cc43db937cab4ff8120
Keerthana Vignesh <keerthuint@gmail.com>
2024-10-02 14:57:52 +0400

    as per review comment made the changes

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,7 +1,15 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
 	
 	int i =20;
+	private By cart = By.id("testing");
+	
+	public void getcart()
+	{
+		System.out.println("cart is added");
+	}
 }
